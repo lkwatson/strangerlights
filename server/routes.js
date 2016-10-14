cdaec24311c603e37b09d66f604ccee2f5e528f2
Ubuntu <ubuntu@ip-172-31-34-160.us-west-2.compute.internal>
2016-10-14 15:38:00 +0000

    Initial files, new html

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,25 @@
-var http = require("http");
+var http = require('http');
 var config = require('./config');
+var mongoose = require('mongoose');
+
+var Messages = require('./messages_model');
+
+var rateLimit = require('express-rate-limit');
+
+var useLimiter = rateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 1,              // 1 request per window  
+  delayMs: 100         // 100ms request delay
+});
 
 module.exports = function(app) {	
 	// Routes
 	
 	app.get('/', function(req, res){
+		res.json(Date.now());	
+  });
+  
+  app.post('/contact-other-side', useLimiter, function(req, res){
 		res.json(Date.now());	
   });
   
--- server/server.js
+++ server/server.js
@@ -3,11 +3,22 @@ var express         = require('express');
 var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
+var mongoose        = require('mongoose');
 
 var port            = 80;
 var app             = express();
 
 var config = require('./config');
+
+require('./messages_model.js');
+
+mongoose.connect(config.dburl,function(err) {
+  if (err) {
+    console.log(err);
+  }else{
+    console.log("Connected to DB");	
+  }
+});
 
 app.use(morgan(':date[clf] :method :status :url :remote-addr :response-time ms - :res[content-length]',{
 	skip: function (req, res) { return req.query.updateRequest === 'true' }
