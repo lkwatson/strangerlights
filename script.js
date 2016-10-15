c1e403a591a526c77e2d1cd0bb97c9ed080e77c6
Lucas <bigluc23@gmail.com>
2016-10-14 22:09:59 -0400

    Basic frontend with posting to server, display queue

---
+++ script.js
@@ -0,0 +1,70 @@
+var contactForm = document.getElementById("contact-upsidedown-form");
+
+//On hitting the submit button, send to server
+contactForm.addEventListener("submit", function(f){
+  
+  f.preventDefault();
+  var message = document.getElementById("contact-form-message").value;
+  
+  var req = new XMLHttpRequest();
+  
+  //callback for state changes (load, error, etc.)
+  req.onreadystatechange = function (aEvt) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        console.log(req.response);
+        getMessageQueue();
+      }else{
+        console.log(req.response);
+      }
+    }
+  };
+  
+  req.open('POST', 'https://upsidedown.strangerlights.com/willareyouthere', true);
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  req.send("message="+message);
+  
+}, false);
+
+//get the message queue to show on the main page
+function getMessageQueue() {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'https://upsidedown.strangerlights.com/messagequeue', true);
+  
+  req.onload = function() {
+    if (req.status > 199 && req.status < 400) {
+      // Success!
+      var data = JSON.parse(req.response);
+      console.log(data);
+      console.log(data.length)
+      populateMessageQueueTable(data);
+    } else {
+      console.log(req)
+    }
+  };
+  req.onerror = function() {
+    console.log("Connection error");
+  };
+  
+  req.send();
+}
+
+function populateMessageQueueTable(data) {
+  var table = document.getElementById("message-queue-table");
+  
+  for (var i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
+  
+  for (i = 0; i < data.length; i++) {
+    var newRow = table.insertRow(i+1);
+    var msgCell = newRow.insertCell(0);
+    var stsCell = newRow.insertCell(1);
+    var ageCell = newRow.insertCell(2);
+    msgCell.innerHTML = data[i].message;
+    stsCell.innerHTML = data[i].displayed;
+    ageCell.innerHTML = data[i].dateSent;
+  }
+}
+
+getMessageQueue();
