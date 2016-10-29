4a91a55960c82b537dfb147937d26d3c17b25015
Lucas <bigluc23@gmail.com>
2016-10-28 23:44:47 -0400

    Added filtering for explicit stuff

--- script.js
+++ script.js
@@ -7,23 +7,33 @@ contactForm.addEventListener("submit", function(f){
   f.preventDefault();
   var message = contactInput.value;
   
-  var req = new XMLHttpRequest();
+  if (message.match(/^\s+$/) || message.length == 0) { //if only spaces present, tell the user
+    document.getElementById("message-user-error-spaces").style.display = "block";
+  }else if(message.length > 21) {
+    document.getElementById("message-user-error-toolong").style.display = "block";
+  }else if(testForNaughtyStuff(message)) {
+    document.getElementById("message-user-error-naughty").style.display = "block";
+  }else{//if the message is okay, send it
   
-  //callback for state changes (load, error, etc.)
-  req.onreadystatechange = function (aEvt) {
-    if (req.readyState == 4) {
-      if(req.status == 200) {
-        console.log(req.response);
-        getMessageQueue();
-      }else{
-        console.log(req.response);
+    var req = new XMLHttpRequest();
+    
+    //callback for state changes (load, error, etc.)
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState == 4) {
+        if(req.status == 200) {
+          console.log(req.response);
+          getMessageQueue();
+        }else{
+          console.log(req.response);
+        }
       }
-    }
-  };
-  
-  req.open('POST', 'https://upsidedown.strangerlights.com/willareyouthere', true);
-  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  req.send("message="+message);
+    };
+    
+    req.open('POST', 'https://upsidedown.strangerlights.com/willareyouthere', true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    req.send("message="+message);
+    
+  }
   
 }, false);
 
@@ -38,7 +48,7 @@ function getMessageQueue() {
       var data = JSON.parse(req.response);
       populateMessageQueueTable(data);
     } else {
-      console.log(req)
+      console.log(req);
     }
   };
   req.onerror = function() {
@@ -75,7 +85,7 @@ function populateMessageQueueTable(data) {
     
     dateObj = new Date(Date.parse(data[i].dateSent));
     
-    var newRow = table.insertRow(i+1)
+    var newRow = table.insertRow(i+1);
     newRow.classList.add(classToAdd);
     
     var msgCell = newRow.insertCell(0);
@@ -87,10 +97,67 @@ function populateMessageQueueTable(data) {
   }
 }
 
+function testForNaughtyStuff(string) {
+  //These are all naughty words that we don't want to be posted publically
+  //While this code can be circumvented, it's mainly intended for gracefully 
+  //telling the user to be more polite. This code is also active on the server side
+  
+  //Note that for a variety of reasons, the naughty words in question have 
+  //been shifted with a caesar cipher.
+  
+  var naughtyWords = ["kzhp", "xmny", "yny", "hqnytwnx", "{flnsf", "snljw", "snlf", "ujsnx", "mtqthfzxy", "oj|", "ywzru", "mnqfw~", "hqnsyts", "gttgx", "fwxj", "gnyhm", "gfxyfwi", "gtsjw", "gzyy", "hthp", "htts", "hzr", "hzsy", "inqit", "jofhzqfyj", "kfl", "kflty", "kflty", "khzp", "kjqqfy", "kzp", "mtws~", "on", "on", "qfgnf", "rfxyjwgfyj", "rfxyjwgfynts", "twlfxr", "umzp", "unxx", "utws", "uzxx~", "wjyfwi", "xj}", "xjrjs", "xrjlrf", "{zq{f", "|fsp", "|mtwj", "mnyqjw", "sfn", "gtrg", "lzs", "snll"];
+  var naughtyWordsWRepeat = ["fxx","snlljw", "snllf","snll","ppp"];
+  //uncomment to create a new Caesar array
+  /*
+  newArray = []
+  for(j = 0; j < naughtyWords.length; j++) {
+    newArray[j] = caesar(naughtyWords[j],5);
+  }
+  */
+  stringWhole = string.toLowerCase().replace(/\s/g, '');
+  string = string.toLowerCase().replace(/\s/g, '').replace(/(.)\1{1,}/g, '$1');
+  console.log(string);
+  
+  for(j = 0; j < naughtyWords.length; j++) {
+    var wordToTest = caesar(naughtyWords[j],-5);
+    
+    if(string.indexOf(wordToTest) >= 0) {
+      console.log(wordToTest);
+      return true;
+      break;
+    }
+  }
+  for(k = 0; k < naughtyWordsWRepeat.length; k++) {
+    var wordToTest = caesar(naughtyWordsWRepeat[k],-5);
+    
+    if(stringWhole.indexOf(wordToTest) >= 0) {
+      console.log(wordToTest);
+      return true;
+      break;
+    }
+  }
+  
+  return false;
+}
+
+function caesar(str,shift) {
+  result = '';
+  for (i = 0; i < str.length; i++) {
+    ccode = (str[i].charCodeAt()) + shift;
+    result += String.fromCharCode(ccode);
+  }
+  return result;
+}
+
 contactInput.addEventListener('keyup', function(event) {
+  document.getElementById("message-user-error-toolong").style.display = "none";
+  document.getElementById("message-user-error-spaces").style.display = "none";
+  document.getElementById("message-user-error-naughty").style.display = "none";
+  
   if (!contactInput.value.match(/[A-Za-z ]+$/)) {
     contactInput.value = contactInput.value.replace(/[^A-Za-z ]+$/g, '');
   }
 });
 
 getMessageQueue();
+setInterval(function(){ getMessageQueue(); }, 5000);
