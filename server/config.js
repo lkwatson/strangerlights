56d9a2c58e48aff1a599005ee04467f3e518fd54
Ubuntu <ubuntu@ip-172-31-34-160.us-west-2.compute.internal>
2016-11-04 22:29:54 +0000

    Added copyright

--- server/config.js
+++ server/config.js
@@ -1,3 +1,13 @@
+/************************
+ * Stranger Lights Server. Strangerlights.com
+ * 
+ * A halloween IoT project. Aphabet light board that displays messages from a server.
+ * 
+ * Copyright 2016 Lucas Watson
+ * 
+ * Released under GPL-3.0
+************************/
+
 module.exports = {
   dburl : "127.0.0.1"+":27017",
   iotPassword : "this-should-be-changed"
